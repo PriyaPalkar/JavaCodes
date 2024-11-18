215e83078239dc10f8552b8ea66d55d4a77f1768
Priya Palkar <153115859+PriyaPalkar@users.noreply.github.com>
2024-11-19 02:22:21 +0530

    Update RemoveDuplicate.java

--- RemoveDuplicate.java
+++ RemoveDuplicate.java
@@ -2,7 +2,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
-public class RemoveDuplicate {
+public class RemoveDuplicat {
     public static void main(String[] args) {
         ArrayList<Integer> arr = new ArrayList<>();
         arr.add(10);
